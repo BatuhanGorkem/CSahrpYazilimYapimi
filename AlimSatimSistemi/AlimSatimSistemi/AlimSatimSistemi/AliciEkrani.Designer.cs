b93cda70aa95653296adc1f0947f2d0fe3bf274a
BatuhanGorkem <batuhangorkem_8@hotmail.com>
2021-05-17 18:51:38 +0300

    Alım satım işlemleri
    Yazılım Yapımı Proje c#

---
+++ AlimSatimSistemi/AlimSatimSistemi/AlimSatimSistemi/AliciEkrani.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace AlimSatimSistemi
+{
+    partial class AliciEkrani
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbtalep = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgurunler = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgalistalepleri = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.bakiyelbl = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgurunler)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgalistalepleri)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbtalep);
+            this.groupBox1.Location = new System.Drawing.Point(12, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(244, 85);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bakiye Talebi";
+            // 
+            // tbtalep
+            // 
+            this.tbtalep.Location = new System.Drawing.Point(113, 19);
+            this.tbtalep.Name = "tbtalep";
+            this.tbtalep.Size = new System.Drawing.Size(100, 20);
+            this.tbtalep.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Miktar:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "₺";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Talep Gönder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 109);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(244, 110);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ürün Alış Talebi";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(111, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Ürün Tipi";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(111, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Talep Gönder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Miktar";
+            // 
+            // dgurunler
+            // 
+            this.dgurunler.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgurunler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgurunler.Location = new System.Drawing.Point(262, 25);
+            this.dgurunler.Name = "dgurunler";
+            this.dgurunler.Size = new System.Drawing.Size(270, 235);
+            this.dgurunler.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(262, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Ürünleriniz";
+            // 
+            // dgalistalepleri
+            // 
+            this.dgalistalepleri.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgalistalepleri.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgalistalepleri.Location = new System.Drawing.Point(538, 25);
+            this.dgalistalepleri.Name = "dgalistalepleri";
+            this.dgalistalepleri.Size = new System.Drawing.Size(250, 235);
+            this.dgalistalepleri.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(535, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Alış Talepleriniz";
+            // 
+            // bakiyelbl
+            // 
+            this.bakiyelbl.AutoSize = true;
+            this.bakiyelbl.Location = new System.Drawing.Point(9, 2);
+            this.bakiyelbl.Name = "bakiyelbl";
+            this.bakiyelbl.Size = new System.Drawing.Size(58, 13);
+            this.bakiyelbl.TabIndex = 14;
+            this.bakiyelbl.Text = "Bakiyeniz :";
+            // 
+            // AliciEkrani
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 274);
+            this.Controls.Add(this.bakiyelbl);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dgalistalepleri);
+            this.Controls.Add(this.dgurunler);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "AliciEkrani";
+            this.Text = "Alıcı İşlemleri";
+            this.Load += new System.EventHandler(this.AliciEkrani_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgurunler)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgalistalepleri)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbtalep;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgurunler;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgalistalepleri;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label bakiyelbl;
+    }
+}
---
+++ AlimSatimSistemi/AlimSatimSistemi/AlimSatimSistemi/Takas.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlimSatimSistemi
+{
+    class Takas
+    {
+        private static Talep SatisTalebiBul(Urun urun)
+        {
+            BorsavtDb db = new BorsavtDb();
+            Talep aranan = null;
+            foreach (Talep talep in db.Talepler.Where(x => x.TalepTuru == "Satış" && x.Urun.UrunAdi == urun.UrunAdi))
+            {
+                if (aranan == null)
+                {
+                    aranan = talep;
+                }
+                else
+                {
+                    if (talep.BirimFiyat > aranan.BirimFiyat)
+                    {
+                        aranan = talep;
+                    }
+                }
+            }
+            return aranan;
+        }
+        public static void TakaslariGerceklestir()
+        {
+            BorsavtDb db = new BorsavtDb();
+            foreach (Talep talep in db.Talepler.Where(x => x.TalepTuru == "Alış" && x.Miktar>0))
+            {
+                Talep talepara = SatisTalebiBul(talep.Urun);
+                Kullanici alici = talep.Kullanici;
+                if (talepara != null&&alici.Bakiye>talepara.BirimFiyat)
+                {
+                    Kullanici satici = db.Kullanicilar.Find(talepara.Kullaniciadi);
+                    KullaniciUrun alinanUrun = null;
+                    int? alinacakMiktar;
+                    if (talepara.Miktar >= talep.Miktar)
+                    {
+                        alinacakMiktar = talep.Miktar;
+                    }
+                    else
+                    {
+                        alinacakMiktar = talepara.Miktar;
+                    }
+                    int? toplamTutar = alinacakMiktar * talepara.BirimFiyat;
+                    if (alici.Bakiye < toplamTutar)
+                    {
+                        alinacakMiktar = (int?)(alici.Bakiye / talepara.BirimFiyat);
+                        toplamTutar = alinacakMiktar * talepara.BirimFiyat;
+                    }
+                    alinanUrun = new KullaniciUrun();
+                    alinanUrun.Miktar = alinacakMiktar;
+                    alinanUrun.Urunid = talepara.Urunid;
+                    alinanUrun.KullaniciAdi = alici.KullaniciAdi;
+                    alinanUrun.Onay = 1;
+                    KullaniciUrun kullanicininUrunu = alici.KullaniciUrunleri.Where(x => x.Urunid == talepara.Urunid).FirstOrDefault();
+                    if (kullanicininUrunu!=null)
+                    {
+                        kullanicininUrunu.Miktar += alinacakMiktar;
+                    }
+                    else
+                    {
+                        alici.KullaniciUrunleri.Add(alinanUrun);
+                    }
+                    alici.Bakiye -= toplamTutar;
+                    satici.Bakiye += toplamTutar;
+                    talepara.Miktar -= alinacakMiktar;
+                    talep.Miktar -= alinacakMiktar;
+                    Islem islem = new Islem();
+                    islem.IslemZamani = DateTime.Now;
+                    islem.Detay = alici.Ad + " " + alinacakMiktar + " kilo " + talep.Urun.UrunAdi + " almak ister ise " + talepara.BirimFiyat + " tl'den alım işlemi gerçekleşti.";
+                    islem.Tutar = alici.Ad + " " + satici.Ad + "'ın hesabına " + toplamTutar + " tl para gönderdi.";
+                    islem.KalanTutar = alici.Ad + " " + alici.Bakiye + " tl parası kaldı.";
+                    islem.BirimFiyat = talepara.BirimFiyat + " tl";
+                    db.Islemler.Add(islem);
+                }
+            }
+            db.SaveChanges();
+            List<Talep> talepler = db.Talepler.Where(x => x.Miktar == 0).ToList();
+            foreach (Talep silinecekTalep in talepler)
+            {
+                db.Talepler.Remove(db.Talepler.Find(silinecekTalep.Id));
+            }
+            List<KullaniciUrun> silinecekUrunler = db.KullaniciUrunleri.Where(x => x.Miktar == 0 && x.Onay == 1).ToList();
+            foreach (KullaniciUrun silinecek in silinecekUrunler)
+            {
+                db.KullaniciUrunleri.Remove(db.KullaniciUrunleri.Find(silinecek.Id));
+            }
+            db.SaveChanges();
+        }
+    }
+}
